514b7152fc8e4819aa3a26dae22125a98ff16754
mayron <mayronreislacerda@gmail.com>
2020-05-01 17:57:32 -0300

    git commit -m "code improvement"

---
+++ src/models/Product.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate');
+
+const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  createsAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+productSchema.plugin(mongoosePaginate);
+mongoose.model("Product", productSchema);
